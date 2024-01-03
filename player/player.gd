ae79f2669d89dcb04926d88c059088fea5d78d0c
Pawan <bilkhu.pawan@gmail.com>
2024-01-02 16:15:26 -0800

    Created a base level using tilemaps, created player that moves, animates and collides with the tilemap

---
+++ player/player.gd
@@ -0,0 +1,96 @@
+extends CharacterBody2D
+
+class_name Player
+
+
+@onready var sound_player = $SoundPlayer
+@onready var debug_label = $DebugLabel
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
+
+
+const GRAVITY: float = 1000.0
+const RUN_SPEED: float = 120.0
+const MAX_JUMP_VELOCITY: float = 400.0
+const HURT_DURATION: float = 0.3
+const JUMP_VELOCITY: float = -400.0
+
+enum PLAYER_STATE { IDLE, RUN, JUMP, FALL, HURT}
+
+var _state: PLAYER_STATE = PLAYER_STATE.IDLE
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += GRAVITY*delta
+	
+	get_input()
+	move_and_slide()
+	calculate_states()
+	update_debug_label()
+
+
+func update_debug_label() -> void:
+	debug_label.text = "OnFloor: %s \nPlayerState: %s \nVelocity: %.0f, %.0f " % [
+		is_on_floor(),
+		PLAYER_STATE.keys()[_state],
+		velocity.x,
+		velocity.y,
+	]
+
+
+func get_input() -> void:
+	velocity.x = 0
+	if Input.is_action_pressed("left"):
+		velocity.x = -RUN_SPEED
+		sprite_2d.flip_h = true
+	elif Input.is_action_pressed("right"):
+		velocity.x = RUN_SPEED
+		sprite_2d.flip_h = false
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		SoundManager.play_clip(sound_player, SoundManager.SOUND_JUMP)
+	
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_JUMP_VELOCITY )
+
+
+func calculate_states() -> void:
+	if _state == PLAYER_STATE.HURT:
+		return
+	
+	if is_on_floor():
+		if velocity.x == 0:
+			set_state(PLAYER_STATE.IDLE)
+		else:
+			set_state(PLAYER_STATE.RUN)
+	else:
+		if velocity.y > 0:
+			set_state(PLAYER_STATE.FALL)
+		else:
+			set_state(PLAYER_STATE.JUMP)
+
+func set_state(new_state: PLAYER_STATE) -> void:
+	if new_state == _state:
+		return
+	
+	if _state == PLAYER_STATE.FALL:
+		if new_state == PLAYER_STATE.IDLE or new_state == PLAYER_STATE.RUN:
+			SoundManager.play_clip(sound_player, SoundManager.SOUND_LAND)
+	
+	_state = new_state
+	
+	match _state:
+		PLAYER_STATE.IDLE:
+			animation_player.play("idle")
+		PLAYER_STATE.RUN:
+			animation_player.play("run")
+		PLAYER_STATE.JUMP:
+			animation_player.play("jump")
+		PLAYER_STATE.FALL:
+			animation_player.play("fall")
+	
